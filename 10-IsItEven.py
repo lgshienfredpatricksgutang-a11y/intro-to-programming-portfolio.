70f53b1ba2a16dc55f92697ba43322c7e5382c03
lgshienfredpatricksgutang-a11y <lgshienfredpatricksgutang@gmail.com>
2025-10-30 13:25:20 +0400

    Update 10-IsItEven.py

--- 10-IsItEven.py
+++ 10-IsItEven.py
@@ -1,5 +1,5 @@
 
-# this program checks if a number is even or odd
+# This program checks if a number is even or odd
 
 def check_even_odd(number):
     if number % 2 == 0: # if the number is divisible by 2 with no remainder
@@ -7,13 +7,14 @@ def check_even_odd(number):
     else:
         return "Odd"
     
-    # main function to execute the program
+    # The main function to execute the program.
 def main():
     num = int(input("Enter an number: ")) # get user input
     result = check_even_odd(num) # call the function to check even or odd
     print(f"The number {num} is {result}.") # print the result
 
-# start the program
+# starts the program.
 if __name__ == "__main__":
     main()
+
 
