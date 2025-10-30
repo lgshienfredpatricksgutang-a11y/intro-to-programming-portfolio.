c84c71737ff14bd8aacca253d5598f1e389bb61e
lgshienfredpatricksgutang-a11y <lgshienfredpatricksgutang@gmail.com>
2025-10-30 13:15:41 +0400

    Fix indentation and formatting in SimpleSums.py

--- 02-SimpleSums.py
+++ 02-SimpleSums.py
@@ -1,5 +1,6 @@
-
+#SimpleSums program
 num1 = 8 # first variable with integer value 8
 num2 = 10 # second variable with integer value 10
 result = num1 + num2 # adding the two variables
 print(result) # printing the result
+
