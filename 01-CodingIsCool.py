c71ea75520ae174d4fd8e53dcbb230cc076cbf56
lgshienfredpatricksgutang-a11y <lgshienfredpatricksgutang@gmail.com>
2025-10-30 13:15:04 +0400

    Update 01-CodingIsCool.py

--- 01-CodingIsCool.py
+++ 01-CodingIsCool.py
@@ -1,12 +1,13 @@
-# This is a simple Python program that prints out three words: "Coding is cool"
+# This is a simple Python program that prints out three words: "Coding is cool".
 
 word1 = 'Coding'
 word2 = 'is'
 word3 = 'cool'
 
 # each word1, word2, word3 are the stringns while the string values are 'Coding', 'is', and 'cool' respectively.
-# it Out puts the three words with spaces in between them
+# it Out puts the three words with spaces in between them.
 
 print(word1, word2, word3)
-#this code prints the three words 'Coding is cool'
+#this code prints the three words 'Coding is cool'.
+
 
